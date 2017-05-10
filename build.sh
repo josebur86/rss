bf9de47afe2e2b73a02018e77008aeb7dd45a515
Joe Burns <josebur86@gmail.com>
2017-05-10 07:57:47 -0500

    Misc.

--- build.sh
+++ build.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 
+# TODO(joe): Make this build in a build directory.
+
 clang -g -fPIC -shared reader.cpp -Wno-c++11-compat-deprecated-writable-strings -o libreader.so
 clang -g main.cpp -Wno-c++11-compat-deprecated-writable-strings -lcurl -o reader libreader.so -Wl,-rpath,/home/joe/work/rss_reader/ 

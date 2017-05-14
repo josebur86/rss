5b29f437f3ddab3acf43bebd22a7e48330b1bf54
Joe Burns <josebur86@gmail.com>
2017-05-13 21:04:30 -0500

    Build script changes for weird vim behavior.

--- build.sh
+++ build.sh
@@ -4,9 +4,6 @@ if [ ! -d "build" ]; then
     mkdir build
 fi
 
-cd build
+clang -g -fPIC -shared reader.cpp -Wno-c++11-compat-deprecated-writable-strings -lcurl -o ./build/libreader.so
+clang -g main.cpp -Wno-c++11-compat-deprecated-writable-strings -o ./build/reader ./build/libreader.so -Wl,-rpath,/home/joe/work/rss_reader/build 
 
-clang -g -fPIC -shared ../reader.cpp -Wno-c++11-compat-deprecated-writable-strings -lcurl -o libreader.so
-clang -g ../main.cpp -Wno-c++11-compat-deprecated-writable-strings -o reader libreader.so -Wl,-rpath,/home/joe/work/rss_reader/build 
-
-cd ..
